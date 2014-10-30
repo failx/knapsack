469ba1100511224c88fa1e7ea67ed071d812b68a
Georg Seifert <seifertg@thi.de>
2014-10-30 09:37:53 +0100

    Multiplikatives Inveres un erweiterter Euklid hinzugefuegt
    Signed-off-by: Georg Seifert <seifertg@thi.de>

--- knapsack.hs
+++ knapsack.hs
@@ -1,6 +1,7 @@
 import System.Random
 import System.IO
 import System.IO.Unsafe
+import Data.Tuple
 
 -- Datenstruktur fuer eine privaten Schluessel
 data PrivateKey = PrivateKey Int Integer Integer [Integer] deriving (Show) 
@@ -22,7 +23,7 @@ getR (PrivateKey _ _ r _) = r
 
 -- Getter fuer Secret Sequence
 getSecretSequence :: PrivateKey -> [Integer]  
-getSecretSequence (PrivateKey _ _ _ sequence) = sequence
+getSecretSequence (PrivateKey _ _ _ sq) = sq
 
 -- Save Private Key in "private.key"
 -- 1. Zeile: Parameter Q
@@ -38,7 +39,7 @@ savePrivateKey k = do
                    
 -- Getter fuer Public Sequence
 getPublicSequence :: PublicKey -> [Integer]
-getPublicSequence (PublicKey _ sequence) = sequence
+getPublicSequence (PublicKey _ sq) = sq
 
 --Getter fuer Key Size
 getPublicKeySize :: PublicKey -> Int
@@ -57,6 +58,19 @@ euclid :: Integer -> Integer -> Integer
 euclid a 0 = a
 euclid a b = euclid b (a `mod` b)
 
+-- Erweiterter euklidischer Algorithmus
+extEuclid :: Integer -> Integer -> (Integer, Integer, Integer)
+extEuclid a 0 = (a,1,0)
+extEuclid a b = let (d,s,t) = extEuclid b (a `mod` b)
+                in (d,t,s - (a `div` b) * t)
+
+-- Multiplicative inverse
+multInverse :: Integer -> Integer -> Integer
+multInverse a b = let (d,s,t) = extEuclid a b
+                  in if s < 0
+                        then s + b
+                        else s
+
 -- Random Number Generator, basierend auf System.Random
 -- Es ist keine Kryptographischen sicheren Zufahllszahlengenerator
 myRND :: Integer -> Integer -> IO Integer
@@ -71,18 +85,18 @@ rndMinMax a b = unsafePerformIO $ myRND a b
 -- Random Number Generator mit min und 1024-fach von min als max
 -- bei 0 als minimum wird 2^64 als max verwendet
 rndMin :: Integer -> Integer
-rndMin 0 = rndMinMax 0 $ 2^64
+rndMin 0 = rndMinMax 0 $ 2 ^ 64
 rndMin a = rndMinMax a $ (*) a 1024
 
 -- Genereate a Super Increasing Sequence, Summe der vorgerigen Werte wird mit uebergeben
-superinc_help :: Int -> Integer -> [Integer]
-superinc_help 0 _ = []
-superinc_help a b = let x = rndMin b
-                   in  x : superinc_help (a - 1) (b + x)
+superincHelp :: Int -> Integer -> [Integer]
+superincHelp 0 _ = []
+superincHelp a b = let x = rndMin b
+                   in  x : superincHelp (a - 1) (b + x)
 
 -- Generiert Super Increasing Sequence mit Anzahl der Elemente, die generiert werden
 superinc :: Int -> [Integer]
-superinc a = superinc_help a 0
+superinc a = superincHelp a 0
 
 -- Generiere aus der Super Increasing Sequence den Parameter Q
 generateQ :: [Integer] -> Integer
@@ -109,16 +123,18 @@ genPublicKey :: PrivateKey -> PublicKey
 genPublicKey a = PublicKey (getPrivateKeySize a)  (map (`mod` getQ a) $ map (getR a *) (getSecretSequence a))
 
 -- Encrypt and a list (Bitfield) with the Public Key Sequence
-encrypt_help :: [Integer] -> [Integer] -> Integer
-encrypt_help []     []     = 0
-encrypt_help [k]    [t]    = (*) k t
-encrypt_help (k:ks) (t:ts) = (+) ( (*) k t)  (encrypt_help ks ts)
+encryptHelp :: [Integer] -> [Integer] -> Integer
+encryptHelp [] []         = 0
+encryptHelp [] _          = error "Schluessel zu kurz"
+encryptHelp _ []          = error "Bitfield zu kurz"
+encryptHelp [k] [t]       = (*) k t
+encryptHelp (k:ks) (t:ts) = (+) ( (*) k t)  (encryptHelp ks ts)
 
 -- Encrypt a List of 0,1 (Bitfield) with a PublicKey
 encrypt :: PublicKey -> [Integer] -> Integer
 encrypt k t 
             | (getPublicKeySize k) /= (length t) = error "Schluessegroesse ungleich Text"
-            | otherwise                          = encrypt_help (getPublicSequence k) t
+            | otherwise                          = encryptHelp (getPublicSequence k) t
             
 decrypt :: PrivateKey -> Integer -> [Integer]
 decrypt _ _ = error "Nicht Implementiert"
