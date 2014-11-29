2767907ce86fcfcae3c5715d7548c7cca83573c4
Felix Martin <failx@felixm.de>
2014-11-29 18:22:43 +0100

    Wrote cabal file. cabal install works now.

--- Main.hs
+++ Main.hs
@@ -1,3 +1,5 @@
+module Main where
+
 import qualified System.Environment as SE
 import qualified Knapsack as KS
 import qualified Keys as K
@@ -6,28 +8,27 @@ import qualified Data.ByteString.Lazy as BS
 import qualified Data.ByteString.Lazy.Char8 as BS8
 import Crack
 
---main = SE.getArgs >>= handleArgs >>= putStrLn
+main = SE.getArgs >>= handleArgs >>= putStrLn
 
 handleArgs :: [String] -> IO String
-handleArgs = undefined
 --handleArgs ["encrypt", publicKey]  = fmap BS8.unpack (Main.encrypt publicKey)
 --handleArgs ["decrypt", privateKey] = fmap BS8.unpack (Main.decrypt privateKey)
 --handleArgs ["create", name]        = create name
 --handleArgs ["runtests"]            = return . show $ T.test
 --handleArgs ["shell"]               = return "foo"
 --handleArgs ["crack", publicKey]    = return "foo"
---handleArgs _                       = return $ unlines options
---
---options = 
---  [ "clks usage: "
---  , "clks encrypt <public key file>    -- reads from stdin and encrypts to stdout"
---  , "clks decrypt <private key file>   -- reads from stdin and decrypts to stdout"
---  , "clks create <keypair name>        -- creates a public/private key pair"
---  , "clks runtests                     -- runs tests for the different modules"
---  , "clks shell                        -- starts an interactive shell"
---  , "clks crack <public key file>      -- reads from stdin and cracks to stdout"
---  ]
---
+handleArgs _                       = return $ unlines options
+
+options = 
+  [ "clks usage: "
+  , "clks encrypt <public key file>    -- reads from stdin and encrypts to stdout"
+  , "clks decrypt <private key file>   -- reads from stdin and decrypts to stdout"
+  , "clks create <keypair name>        -- creates a public/private key pair"
+  , "clks runtests                     -- runs tests for the different modules"
+  , "clks shell                        -- starts an interactive shell"
+  , "clks crack <public key file>      -- reads from stdin and cracks to stdout"
+  ]
+
 --encrypt :: FilePath -> IO BS.ByteString
 --encrypt p = do
 --  string <- BS.getContents
